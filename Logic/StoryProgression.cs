2a9e228526213316fa32d067a5c9c147111faf52
Zoey Price <zoeywprice@gmail.com>
2021-03-04 17:03:35 -0700

    Initial Commit

---
+++ Logic/StoryProgression.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fractured.World;
+
+namespace Fractured.Logic
+{
+    public static class StoryProgression
+    {
+        private static List<Location> visitedLocations;
+        private static Character[] talkedCharacters;
+
+        public static void ResetStoryProgression()
+        {
+            visitedLocations = new List<Location>();
+        }
+
+        public static bool HasLocationBeenVisited(Location location)
+        {
+            foreach (Location l in visitedLocations) { if (location == l) return true; }
+            visitedLocations.Add(location);
+            return false;
+        }
+    }
+}
